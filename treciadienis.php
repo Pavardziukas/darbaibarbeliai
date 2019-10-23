629781ae49b909fb10093e5648e77e5ef6a82a7d
Pavardziukas <55126024+Pavardziukas@users.noreply.github.com>
2019-10-23 16:16:17 +0300

    Add files via upload

--- treciadienis.php
+++ treciadienis.php
@@ -5,24 +5,27 @@ $drinks = [
     name = Vilkmerges alus,
     price_stock = 3.6,
     discount = 0,
-    img = https://www.beersofeurope.co.uk/images/product/l/VilkmergesVysniuKriek.jpg?t=1561548872,
+    img = 'https://www.beersofeurope.co.uk/images/product/l/VilkmergesVysniuKriek.jpg?t=1561548872',
 ]
 [
     name = Stumbro degtinela,
     price_stock = 11.79,
     discount = 0, //%
-    img = https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0w_TuaAQxlMtzZIu0l591pZC0KmgSrtFeW8fPp3rgvSrKvkmG,
+    img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT0w_TuaAQxlMtzZIu0l591pZC0KmgSrtFeW8fPp3rgvSrKvkmG',
 ]
 [
     name = Ballentines,
     price_stock = 14.95,
     discount = 0,
-    img = https://img.thewhiskyexchange.com/900/blend_bal5.jpg,
+    img = 'https://img.thewhiskyexchange.com/900/blend_bal5.jpg',
 ]
 [
     name = Bintang beer,
     price_stock = 2.19,
     discount = 3, //%
-    img = https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQWXjdGoJVzrxGszvR3ncGWgciKVNPPB0JmSaSDqkUWYVrV9y5b,
+    img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQWXjdGoJVzrxGszvR3ncGWgciKVNPPB0JmSaSDqkUWYVrV9y5b',
 ]
 ]
+foreach ($drinks as $key = $value)
+$discounted_price = $value ['$price_stock'] * ((100 - $value['discount'])/100)
+var_dump($drinks)
